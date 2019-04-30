332b83d266dc323cedcfc35b1d314e011f7a6802
Maxwell Kepler <starboundshep@gmail.com>
2019-04-30 14:50:56 +0100

    Models / Textures / AI / Code
    Added AI
    Added cookie model / textures / code
    Added cookie launcher model / texture

---
+++ Assets/Scripts/CookieBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieBullet : MonoBehaviour
+{
+    public float bulletSpeed;
+    private float expirationTimer;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += transform.forward * Time.deltaTime * bulletSpeed;
+        expirationTimer += Time.deltaTime;
+        if (expirationTimer > 2.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/CookieFire.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieFire : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject firePos;
+    public float fireSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, firePos.transform.position, firePos.transform.rotation);
+            //Debug.Log("Click!");
+        }
+    }
+}
