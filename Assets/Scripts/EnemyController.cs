9f6ed26c16b20091d9c44888c5287108222b9ad4
Maxwell Kepler <starboundshep@gmail.com>
2019-05-01 16:06:48 +0100

    More AI / new textures / enemys now shoot

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,11 +6,15 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     public GameObject player;
-    public float maxCloseness;
-    public int hitPoints;
-    public float reloadTime;
-    public float shootDistance;
+    public float maxCloseness;      // Distance at which enemy will stop when too close
+    public int hitPoints;           // HP
+    public float reloadTime;        // Fire speed
+    public GameObject bullet;       // Bullet object
+    public float shootDistance;     // Max distnace at which enemy will shoot
+    public GameObject sparks;       // Death particles
+    public GameObject firePos;      // Where to fire bullets from
     private float reloadTimer;
+    private bool isDead = false;
     NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -18,28 +22,42 @@ public class EnemyController : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         agent.stoppingDistance = maxCloseness;
+        reloadTimer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        var rayPosition = player.transform.position - transform.position;
-        if (Physics.Raycast (transform.position, rayPosition, out hit))
-        {
-            if (hit.transform == player.transform)
-            {
-                agent.SetDestination(player.transform.position);
-                transform.LookAt(player.transform, Vector3.up);
-                //transform.Rotate(0.0f, transform.rotation.y, 0.0f);
-                //Quaternion rotation = Quaternion.LookRotation(player.transform.position);
-                //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 200);
-            }
-            //Debug.DrawRay(transform.position, rayPosition, Color.yellow);
-        }
         if (hitPoints < 0)
         {
-            Destroy(gameObject);
+            if (isDead == false)
+            {
+                Instantiate(sparks, transform.position, transform.rotation);
+                isDead = true;
+            }
+        }
+        else
+        {
+            RaycastHit hit;
+            var rayPosition = player.transform.position - transform.position;
+            if (Physics.Raycast(transform.position, rayPosition, out hit))
+            {
+                if (hit.transform == player.transform)
+                {
+                    agent.SetDestination(player.transform.position);
+                    transform.LookAt(player.transform, Vector3.up);
+                    if (reloadTimer > reloadTime)
+                    {
+                        Instantiate(bullet, firePos.transform.position, firePos.transform.rotation);
+                        reloadTimer = 0.0f;
+                    }
+                    reloadTimer += Time.deltaTime;
+                    //transform.Rotate(0.0f, transform.rotation.y, 0.0f);
+                    //Quaternion rotation = Quaternion.LookRotation(player.transform.position);
+                    //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 200);
+                }
+                //Debug.DrawRay(transform.position, rayPosition, Color.yellow);
+            }
         }
     }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,26 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public int startingHealth = 100;
     public Text healthText;
+    private int hitPoints = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        healthText.text = "Health: " + startingHealth.ToString();
+        healthText.text = "Health: " + hitPoints.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = "Health: " + startingHealth.ToString();
+        healthText.text = "Health: " + hitPoints.ToString();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            hitPoints -= 4;
+        }
     }
 }
